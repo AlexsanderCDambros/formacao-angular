881390499eb74b599da7f3f581afc639e5ee69ff
AlexsanderCDambros <alexsanderdambros@gmail.com>
2023-01-29 16:28:35 -0300

    feat: listar tarefas

---
+++ projetos/gerenciador-de-tarefas/src/app/tarefas/listar/listar-tarefa.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { Tarefa, TarefaService } from '../shared';
+
+@Component({
+  selector: 'app-listar-tarefa',
+  templateUrl: './listar-tarefa.component.html',
+  styleUrls: ['./listar-tarefa.component.scss']
+})
+export class ListarTarefaComponent {
+
+  tarefas: Tarefa[] = [];
+
+  constructor(private tarefaService: TarefaService) {}
+
+  ngOnInit() {
+  	this.tarefas = this.listarTodos();
+    // this.tarefas = [
+    //   { id: 1, nome: "teste 1 ", concluida: false},{ id: 2, nome: "teste 2 ", concluida: true}
+    // ]
+  }
+
+  listarTodos(): Tarefa[] {
+  	return this.tarefaService.listarTodos();
+  }
+
+  remover($event: any, tarefa: Tarefa): void {
+    $event.preventDefault();
+    if (confirm('Deseja remover a tarefa "' + tarefa.nome + '"?')) {
+      this.tarefaService.remover(tarefa.id || 0);
+      this.tarefas = this.listarTodos();
+    }
+  }
+
+  alterarStatus(tarefa: Tarefa): void {
+    if (confirm('Deseja alterar o status da tarefa "' + tarefa.nome + '"?')) {
+      this.tarefaService.alterarStatus(tarefa.id || 0);
+      this.tarefas = this.listarTodos();
+    }
+  }
+
+}
